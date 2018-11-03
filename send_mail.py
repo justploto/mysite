d53c4a402a3152fcdd561b2154a348e719e8c186
pluto <251658581@qq.com>
2018-11-03 21:42:37 +0800

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     send_mail(
         '测试邮件',
         '欢迎访问www.liujiangblog.com，这里是刘江的博客和教程站点，本站专注于Python和Django技术的分享！',
-        '15395056696@189.cn',
-        ['15395056696@189.cn'],
+        'xxx@189.cn',
+        ['xxx@189.cn'],
     )
